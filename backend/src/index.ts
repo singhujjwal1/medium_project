a361157c828a47a2fa4335c620426fcb7c9a4a6d
singhujjwal1 <ujjwalsinh25@gmail.com>
2024-06-09 23:49:09 +0530

    db setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono'
-
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 const app = new Hono()
 
 app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+  }).$extends(withAccelerate())
   return c.text('Hello Hono!')
 })
 app.post('/api/v1/signin', (c) => {
